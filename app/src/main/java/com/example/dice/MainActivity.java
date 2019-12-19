1b4a3aeff0dae485b9810135fb9ef7facade05a2
ashleygoury <e1512090@cmaisonneuve.qc.ca>
2019-12-18 21:50:09 -0500

    Change to 12 dices

--- app/src/main/java/com/example/dice/MainActivity.java
+++ app/src/main/java/com/example/dice/MainActivity.java
@@ -1,28 +1,16 @@
 package com.example.dice;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.NumberPicker;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -31,11 +19,12 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    int score, dice1, dice2, dice3;
-    ArrayList dice;
-    ArrayList<ImageView> imageViews;
-    Random random;
-    TextView scoretxt;
+    int[] diceArray = new int[11];
+    public int[] imgdice = {R.id.die1Image, R.id.die2Image, R.id.die3Image, R.id.die4Image, R.id.die5Image,
+            R.id.die6Image, R.id.die7Image, R.id.die8Image, R.id.die9Image, R.id.die10Image, R.id.die11Image, R.id.die12Image};
+    ArrayList dice = new ArrayList();
+    ArrayList<ImageView> imageViews = new ArrayList<>();
+    Random random = new Random();
 
 
     @Override
@@ -43,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        show();
+//        show();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -56,80 +45,56 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        score = 0;
-
-        random = new Random();
-        dice = new ArrayList();
-        scoretxt = findViewById(R.id.txtScore);
-
-        ImageView die1Image = findViewById(R.id.die1Image);
-        ImageView die2Image = findViewById(R.id.die2Image);
-        ImageView die3Image = findViewById(R.id.die3Image);
-
-        imageViews = new ArrayList<>();
-        imageViews.add(die1Image);
-        imageViews.add(die2Image);
-        imageViews.add(die3Image);
+        for (int i = 0; i < 11; i++) {
+            imageViews.add((ImageView) findViewById(imgdice[i]));
+        }
     }
 
-    public void show() {
-        final AlertDialog.Builder d = new AlertDialog.Builder(MainActivity.this);
-        LayoutInflater inflater = this.getLayoutInflater();
-        View dialogView = inflater.inflate(R.layout.number_picker_dialog, null);
-        d.setTitle("Dice game");
-        d.setMessage("How many dice do you want?");
-        d.setView(dialogView);
-        final NumberPicker numberPicker = (NumberPicker) dialogView.findViewById(R.id.dialog_number_picker);
-        numberPicker.setMaxValue(9);
-        numberPicker.setMinValue(1);
-        numberPicker.setWrapSelectorWheel(true);
-
-        d.setPositiveButton("Done", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Log.d("TAG", "onClick: " + numberPicker.getValue());
-            }
-        });
-
-        AlertDialog alertDialog = d.create();
-        alertDialog.show();
-    }
+//    public void show() {
+//        final AlertDialog.Builder d = new AlertDialog.Builder(MainActivity.this);
+//        LayoutInflater inflater = this.getLayoutInflater();
+//        View dialogView = inflater.inflate(R.layout.number_picker_dialog, null);
+//        d.setTitle("Dice game");
+//        d.setMessage("How many dice do you want?");
+//        d.setView(dialogView);
+//        final NumberPicker numberPicker = (NumberPicker) dialogView.findViewById(R.id.dialog_number_picker);
+//        numberPicker.setMaxValue(9);
+//        numberPicker.setMinValue(1);
+//        numberPicker.setWrapSelectorWheel(true);
+//
+//        d.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                Log.d("TAG", "onClick: " + numberPicker.getValue());
+//            }
+//        });
+//
+//        AlertDialog alertDialog = d.create();
+//        alertDialog.show();
+//    }
 
     public void rollDice(View view) {
-        dice1 = random.nextInt(6)+1;
-        dice2 = random.nextInt(6)+1;
-        dice3 = random.nextInt(6)+1;
+        for (int i = 0; i < 11; i++) {
+            diceArray[i] = random.nextInt(6) + 1;
+        }
 
         dice.clear();
-        dice.add(dice1);
-        dice.add(dice2);
-        dice.add(dice3);
 
-        for (int dieOfSet = 0; dieOfSet < 3; dieOfSet++){
+        for (int i = 0; i < 11; i++) {
+            dice.add(diceArray[i]);
+        }
+
+        for (int dieOfSet = 0; dieOfSet < 11; dieOfSet++) {
             String imageName = "die_" + dice.get(dieOfSet) + ".png";
             try {
                 InputStream stream = getAssets().open(imageName);
-                Drawable d = Drawable.createFromStream(stream,null);
+                Drawable d = Drawable.createFromStream(stream, null);
                 imageViews.get(dieOfSet).setImageDrawable(d);
-            } catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
-        String msg;
-
-        if(dice1 == dice2 && dice1 ==  dice3){
-            int scoreDelta = dice1 * 100;
-            score += scoreDelta;
-            msg = "TRIPLE = " + scoreDelta + "\n" + "POINTS = " + score;
-        } else if(dice1 ==  dice2 || dice1 == dice3 || dice2 == dice3){
-            msg = "DOUBLE = 50" + "\n" + "PONITS = " + score;
-            score += 50;
-        } else {
-            msg = "No win" + "\n" + "POINTS = " + score;
-        }
-
-        scoretxt.setText(msg);
     }
 
     @Override
